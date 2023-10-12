9a281dccdcf8565ed28e5745499f101061a6c90d
Baris Can Berki <av.bariscanberki@gmail.com>
2023-10-12 22:41:45 +0300

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int r;
+        double pi = 3.14;
+        Scanner inp = new Scanner(System.in);
+        System.out.print(" Input the r for calculation : ");
+        r = inp.nextInt();
+        double area = pi * r * r;
+        double perimeter = 2 * pi * r;
+        System.out.println(" Area of the Circle : " + area);
+        System.out.println(" Perimeter of the Circle : " + perimeter);
+    }
+}
